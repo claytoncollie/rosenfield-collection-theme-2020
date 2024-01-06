730792e86ad903eea406618f2ad094a3e063774f
Clayton Collie <clayton.collie@gmail.com>
2024-01-06 14:33:02 -0600

    thumbnail images

--- partials/image-thumbnails.php
+++ partials/image-thumbnails.php
@@ -11,28 +11,29 @@ if ( empty( $images ) ) {
 	return;
 }
 
-$width = $image['sizes']['thumbnail-width'] ?? false;
-$width = $width ? (string) $width : '';
-
-$height = $image['sizes']['thumbnail-height'] ?? false;
-$height = $height ? (string) $height : '';
-
-$source = $image['sizes']['thumbnail'] ?? false;
-$source = $source ? (string) $source : '';
-
-$title = $image['title'] ?? false;
-$title = $title ? (string) $title : '';
-
 ?>
 
 <section class="slider-thumbnails" aria-label="Thumbnail Images">
 	<ul class="slider-thumbnails-images">
-		<?php foreach ( $images as $image ) : ?>
+		<?php 
+		foreach ( $images as $image ) : 
+			$width = $image['sizes']['thumbnail-width'] ?? false;
+			$width = $width ? (string) $width : '';
+
+			$height = $image['sizes']['thumbnail-height'] ?? false;
+			$height = $height ? (string) $height : '';
+
+			$source = $image['sizes']['thumbnail'] ?? false;
+			$source = $source ? (string) $source : '';
+
+			$title = $image['title'] ?? false;
+			$title = $title ? (string) $title : '';
+			?>
 			<li>
 				<img 
 					width="<?php echo esc_attr( $width ); ?>" 
 					height="<?php echo esc_attr( $height ); ?>" 
-					src="<?php echo esc_attr( $source ); ?>" 
+					src="<?php echo esc_url( $source ); ?>" 
 					alt="Thumbnail of <?php echo esc_attr( $title ); ?>"
 				/>
 			</li>
