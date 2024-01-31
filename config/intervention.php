7cf1cd8bf9a6716d217f098484a77a311d45c460
Clayton Collie <clayton.collie@gmail.com>
2024-01-30 19:54:11 -0600

    Allow search engines

--- config/intervention.php
+++ config/intervention.php
@@ -57,6 +57,7 @@ return [
 			'posts-per-page'    => 20,
 			'posts-per-rss'     => 200,
 			'rss-excerpt'       => 'summary',
+			'discourage-search' => false,
 		],
 		'discussion' => [
 			'post.ping-flag'              => 'closed',
